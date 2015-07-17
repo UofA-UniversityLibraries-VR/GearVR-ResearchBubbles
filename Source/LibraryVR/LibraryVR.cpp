8b96afa884e5ddaddf2c826ed248bbf14ef015ce
mrAllWeather <ben.weatherall@gmail.com>
2015-07-17 11:30:47 +0930

    Added Library VR game content

---
+++ Source/LibraryVR/LibraryVR.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "LibraryVR.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LibraryVR, "LibraryVR" );
+
+DEFINE_LOG_CATEGORY(LogLibraryVR)
+ 

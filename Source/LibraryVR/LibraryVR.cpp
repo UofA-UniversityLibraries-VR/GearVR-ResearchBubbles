26cb9bae32cf65274e4d8fbf9a99e7c5223722b5
mrAllWeather <ben.weatherall@gmail.com>
2015-09-09 18:05:41 +0930

    Deleting unused project

--- Source/LibraryVR/LibraryVR.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "LibraryVR.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LibraryVR, "LibraryVR" );
-
-DEFINE_LOG_CATEGORY(LogLibraryVR)
- 
